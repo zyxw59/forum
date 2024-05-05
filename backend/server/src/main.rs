28f68e64322e451ede85e2a8d18f16ae5b95e77e
emily Crandall Fleischman <zyxw59@gmail.com>
2024-05-05 12:53:30 -0400

    initial skeleton

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use tracing_actix_web::TracingLogger;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    HttpServer::new(|| App::new().wrap(TracingLogger::default()).service(root))
+        .bind(("0.0.0.0", 3000))?
+        .run()
+        .await?;
+    Ok(())
+}
+
+#[actix_web::get("/")]
+async fn root() -> &'static str {
+    "hello world"
 }
