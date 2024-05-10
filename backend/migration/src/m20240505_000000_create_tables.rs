8b8e4ad94b6948351349b6fd906fe38e5faec73a
emily Crandall Fleischman <zyxw59@gmail.com>
2024-05-09 21:26:35 -0400

    build the initial migration the "right way"

--- backend/migration/src/m20240505_000000_create_tables.rs
+++ backend/migration/src/m20240505_000000_create_tables.rs
@@ -1,7 +1,4 @@
-use sea_orm::Schema;
 use sea_orm_migration::prelude::*;
-
-mod entity;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -9,17 +6,65 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let db = manager.get_database_backend();
-        let schema = Schema::new(db);
         manager
-            .create_table(schema.create_table_from_entity(entity::forum::Entity))
+            .create_table(
+                Table::create()
+                    .table(Forum::Table)
+                    .col(
+                        ColumnDef::new(Forum::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Forum::Title).string().not_null())
+                    .take(),
+            )
             .await?;
         manager
-            .create_table(schema.create_table_from_entity(entity::thread::Entity))
+            .create_table(
+                Table::create()
+                    .table(Thread::Table)
+                    .col(
+                        ColumnDef::new(Thread::Id)
+                            .big_integer()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Thread::Forum).integer().not_null())
+                    .col(ColumnDef::new(Thread::Title).string().not_null())
+                    .take(),
+            )
             .await?;
         manager
-            .create_table(schema.create_table_from_entity(entity::post::Entity))
+            .create_foreign_key(
+                ForeignKey::create()
+                    .from(Forum::Table, Forum::Id)
+                    .to(Thread::Table, Thread::Id)
+                    .take(),
+            )
             .await?;
+        manager
+            .create_table(
+                Table::create()
+                    .table(Post::Table)
+                    .col(ColumnDef::new(Post::Thread).big_integer().not_null())
+                    .col(ColumnDef::new(Post::Id).small_integer().not_null())
+                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Post::Date).date_time().not_null())
+                    .primary_key(Index::create().col(Post::Thread).col(Post::Id))
+                    .take(),
+            )
+            .await?;
+        manager
+            .create_foreign_key(
+                ForeignKey::create()
+                    .from(Post::Table, Post::Thread)
+                    .to(Thread::Table, Thread::Id)
+                    .take(),
+            )
+            .await?;
+
         Ok(())
     }
 
@@ -27,12 +72,36 @@ impl MigrationTrait for Migration {
         manager
             .drop_table(
                 Table::drop()
-                    .table(entity::forum::Entity)
-                    .table(entity::thread::Entity)
-                    .table(entity::post::Entity)
+                    .table(Forum::Table)
+                    .table(Thread::Table)
+                    .table(Post::Table)
                     .take(),
             )
             .await?;
         Ok(())
     }
+}
+
+#[derive(DeriveIden)]
+enum Forum {
+    Table,
+    Id,
+    Title,
+}
+
+#[derive(DeriveIden)]
+enum Thread {
+    Table,
+    Id,
+    Forum,
+    Title,
+}
+
+#[derive(DeriveIden)]
+enum Post {
+    Table,
+    Id,
+    Thread,
+    Text,
+    Date,
 }
